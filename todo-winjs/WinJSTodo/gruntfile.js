75cff5eb4dded53c31e8b0f4721f97ab682421b3
unknown <ednorieg@EdCordova.redmond.corp.microsoft.com>
2015-06-03 14:10:53 -0700

    retrieving .d.ts files using npm + grunt

---
+++ todo-winjs/WinJSTodo/gruntfile.js
@@ -0,0 +1,18 @@
+﻿/// <binding ProjectOpened='tsd' />
+module.exports = function (grunt) {
+    // Project configuration
+    grunt.initConfig({
+        tsd: {
+            refresh: {
+                options: {
+                    command: 'reinstall',
+                    latest: true,
+                    config: './tsd.json'
+                }
+            }
+        }
+    })
+
+    // Load the "tsd" task to automate TypeScript definition related tasks.
+    grunt.loadNpmTasks('grunt-tsd');
+};
