5483d203565d27e853cfedec8bb976f0c622c219
unknown <ednorieg@EdCordova.redmond.corp.microsoft.com>
2015-06-04 10:43:52 -0700

    Retrieving winjs dependencies using bower

---
+++ todo-winjs/WinJSTodo/gulpfile.js
@@ -0,0 +1,9 @@
+﻿/// <binding ProjectOpened='winjs' />
+var gulp = require("gulp");
+
+gulp.task("winjs", function () {
+    gulp.src(["./bower_components/winjs/js/base.min.js"]).pipe(gulp.dest("./www/scripts/frameworks/"));
+    gulp.src(["./bower_components/winjs/js/ui.min.js"]).pipe(gulp.dest("./www/scripts/frameworks/"));
+    gulp.src(["./bower_components/winjs/css/ui-dark.css"]).pipe(gulp.dest("./www/css/"));
+    gulp.src(["./bower_components/winjs/css/ui-light.css"]).pipe(gulp.dest("./www/css/"));
+});
--- todo-winjs/WinJSTodo/scripts/index.ts
+++ todo-winjs/WinJSTodo/scripts/index.ts
@@ -2,9 +2,9 @@
     Copyright (c) Microsoft. All rights reserved.  Licensed under the MIT license. See LICENSE file in the project root for full license information.
 */
 
-///<reference path='..\..\packages\cordova.TypeScript.DefinitelyTyped.2.3.3\Content\Scripts\typings\cordova\cordova.d.ts' />
-///<reference path='..\..\packages\winjs.TypeScript.DefinitelyTyped.0.1.9\Content\Scripts\typings\winjs\winjs.d.ts'/>
-///<reference path='..\..\packages\azure-mobile-services-client.TypeScript.DefinitelyTyped.0.8.6\Content\Scripts\typings\azure-mobile-services-client\AzureMobileServicesClient.d.ts'/>
+///<reference path='..\typings\cordova\cordova.d.ts' />
+///<reference path='..\typings\winjs\winjs.d.ts'/>
+///<reference path='..\typings\azure-mobile-services-client\AzureMobileServicesClient.d.ts'/>
 /// <reference path="services.ts" />
 /// <reference path="todo.ts" />
 
